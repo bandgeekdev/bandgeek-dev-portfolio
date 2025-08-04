ed5fc68c30773881b2a9f0e222a603178f3b3a57
robinkii <robinkelsickii@gmail.com>
2025-08-03 22:07:54 -0400

    added gradient to the text

--- my-portfolio/src/pages/Home.tsx
+++ my-portfolio/src/pages/Home.tsx
@@ -7,12 +7,17 @@ const Home: React.FC = () => {
         <div className='text-content max-w-2xl w-full px-8 md:px-32'>
           <h1 className='title text-8xl font-mono font-extrabold'>
             Hi
-            <strong className='text-purple-300'>Y'all!</strong>
+            <strong className='bg-[linear-gradient(90deg,_rgba(54,186,149,1)_0%,_rgba(63,209,194,1)_50%,_rgba(222,220,220,1)_100%)] bg-clip-text text-transparent'>
+              Y'all!
+            </strong>
           </h1>
           <p className='mt-4 text-bold text-xl'>
-            I am <span className='text-purple-300'>Robin</span>, a frontend
-            developer based out of Kentucky and I am ready to build sharp,
-            scalable web apps with a creative edge.
+            I am{' '}
+            <span className='bg-[linear-gradient(90deg,_rgba(54,186,149,1)_0%,_rgba(63,209,194,1)_50%,_rgba(222,220,220,1)_100%)] bg-clip-text text-transparent'>
+              Robin
+            </span>
+            , a frontend developer based out of Kentucky and I am ready to build
+            sharp, scalable web apps with a creative edge.
           </p>
         </div>
         <div className='flex flex-col gap-4'>
@@ -20,7 +25,7 @@ const Home: React.FC = () => {
             className='group relative inline-block focus:ring-3 focus:outline-hidden'
             href='#'
           >
-            <span className='absolute inset-0 translate-x-1.5 translate-y-1.5 bg-purple-300 transition-transform group-hover:translate-x-0 group-hover:translate-y-0'></span>
+            <span className='absolute inset-0 translate-x-1.5 translate-y-1.5 bg-[linear-gradient(90deg,_rgba(54,186,149,1)_0%,_rgba(63,209,194,1)_50%,_rgba(222,220,220,1)_100%)] transition-transform group-hover:translate-x-0 group-hover:translate-y-0'></span>
             <span className='relative inline-block w-64 border-2 border-current px-8 py-3 text-sm font-bold tracking-widest text-black'>
               Download Resume/CV
             </span>
@@ -29,7 +34,7 @@ const Home: React.FC = () => {
             className='group relative inline-block focus:ring-3 focus:outline-hidden'
             href='#'
           >
-            <span className='absolute inset-0 translate-x-1.5 translate-y-1.5 bg-purple-300 transition-transform group-hover:translate-x-0 group-hover:translate-y-0'></span>
+            <span className='absolute inset-0 translate-x-1.5 translate-y-1.5 bg-[linear-gradient(90deg,_rgba(54,186,149,1)_0%,_rgba(63,209,194,1)_50%,_rgba(222,220,220,1)_100%)] transition-transform group-hover:translate-x-0 group-hover:translate-y-0'></span>
             <span className='relative inline-block w-64 border-2 border-current px-8 py-3 text-sm font-bold tracking-widest text-black text-center'>
               Contact Me
             </span>
